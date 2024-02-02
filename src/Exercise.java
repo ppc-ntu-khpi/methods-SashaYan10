cc65678b3052f5a67e4fd8b903a333b19857c8ab
Олександр Януш <115615844+SashaYan10@users.noreply.github.com>
2024-02-02 10:35:04 +0000

    Виконана ПР3

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,32 @@
+
 //package domain;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static long Calculate(LocalDate startDate, LocalDate endDate) {
+        long days = 0;
+        LocalDate currenDate = startDate;
+
+        while (!currenDate.isAfter(endDate)) { // перевірка днів у проміжку
+            if (currenDate.getDayOfWeek() != DayOfWeek.SATURDAY && currenDate.getDayOfWeek() != DayOfWeek.SUNDAY) { // якщо
+                                                                                                                    // днем
+                                                                                                                    // не
+                                                                                                                    // являється
+                                                                                                                    // субота
+                                                                                                                    // чи
+                                                                                                                    // неділя,
+                                                                                                                    // то
+                                                                                                                    // збільшується
+                                                                                                                    // кількість
+                                                                                                                    // робочих
+                                                                                                                    // днів
+                days++;
+            }
+
+            currenDate = currenDate.plusDays(1); // перехід до наступного дня
+        }
+
+        return days;
     }
 }
--- src/TestResult.java
+++ src/TestResult.java
@@ -1,12 +1,45 @@
 //package test;
 
 //import domain.Exercise;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;;
 
 public class TestResult {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // підключення об'єкту Scanner для зчитування даних, введеними
+                                                  // користувачем
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+
+        while (startDate == null) { // перевірка на правильність вводу формати дати
+            System.out.println("Вкажіть першу дату: ");
+            String startDateStr = scanner.nextLine();
+
+            try {
+                startDate = LocalDate.parse(startDateStr); // якщо правильно введена дата, то рядок буде переведений в
+                                                           // об'єкт LocalDate
+            } catch (DateTimeParseException e) { // а якщо ні, то потрібно буде ще раз ввести дату до тих пір, поки не
+                                                 // вона не буде введена правильно
+                System.out.println("Невірний формат дати. Спробуйте ще раз!");
+            }
+        }
+
+        while (endDate == null) {
+            System.out.println("Вкажіть другу дату: ");
+            String endDateStr = scanner.nextLine();
+
+            try {
+                endDate = LocalDate.parse(endDateStr);
+            } catch (DateTimeParseException e) {
+                System.out.println("Невірний формат дати. Спробуйте ще раз!");
+            }
+        }
+        scanner.close();
+
+        long days = Exercise.Calculate(startDate, endDate); // виклик функції Exercise
+        System.out.println("кількість робочих днів: " + days);
     }
 }
